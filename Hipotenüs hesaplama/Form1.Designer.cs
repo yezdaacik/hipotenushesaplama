44ced39a7919583904c3c842baba16fa6bb32744
yezdanuracik <145451081+yezdanuracik@users.noreply.github.com>
2023-10-02 13:51:06 +0300

    Add files via upload

---
+++ Hipotenüs hesaplama/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Hipotenüs_hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKenar1 = new System.Windows.Forms.Label();
+            this.lblKenar2 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.txtKenar1 = new System.Windows.Forms.TextBox();
+            this.txtKenar2 = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblKenar1
+            // 
+            this.lblKenar1.AutoSize = true;
+            this.lblKenar1.Location = new System.Drawing.Point(26, 28);
+            this.lblKenar1.Name = "lblKenar1";
+            this.lblKenar1.Size = new System.Drawing.Size(44, 13);
+            this.lblKenar1.TabIndex = 0;
+            this.lblKenar1.Text = "Kenar 1";
+            // 
+            // lblKenar2
+            // 
+            this.lblKenar2.AutoSize = true;
+            this.lblKenar2.Location = new System.Drawing.Point(26, 70);
+            this.lblKenar2.Name = "lblKenar2";
+            this.lblKenar2.Size = new System.Drawing.Size(44, 13);
+            this.lblKenar2.TabIndex = 1;
+            this.lblKenar2.Text = "Kenar 2";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(25, 119);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(55, 20);
+            this.lblSonuc.TabIndex = 2;
+            this.lblSonuc.Text = "Sonuç";
+            // 
+            // txtKenar1
+            // 
+            this.txtKenar1.Location = new System.Drawing.Point(77, 28);
+            this.txtKenar1.Name = "txtKenar1";
+            this.txtKenar1.Size = new System.Drawing.Size(100, 20);
+            this.txtKenar1.TabIndex = 3;
+            // 
+            // txtKenar2
+            // 
+            this.txtKenar2.Location = new System.Drawing.Point(77, 67);
+            this.txtKenar2.Name = "txtKenar2";
+            this.txtKenar2.Size = new System.Drawing.Size(100, 20);
+            this.txtKenar2.TabIndex = 4;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(29, 156);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(148, 48);
+            this.btnHesapla.TabIndex = 5;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtKenar2);
+            this.Controls.Add(this.txtKenar1);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.lblKenar2);
+            this.Controls.Add(this.lblKenar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKenar1;
+        private System.Windows.Forms.Label lblKenar2;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.TextBox txtKenar1;
+        private System.Windows.Forms.TextBox txtKenar2;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ Hipotenüs hesaplama/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hipotenüs_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double kenar1, kenar2, sonuc;
+
+            kenar1 = Convert.ToSingle(txtKenar1.Text);
+            kenar2 = Convert.ToSingle(txtKenar1.Text);
+            sonuc = Math.Sqrt((Math.Pow(kenar1, 2) + Math.Pow(kenar2, 2)));
+
+            lblSonuc.Text = sonuc.ToString();
+        }
+    }
+}
